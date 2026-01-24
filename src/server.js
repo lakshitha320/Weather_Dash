0ee729098fba5da590dc59490b6f6aad4d79e64a
udeeshaakshari-art <udeeshaakshari@gmail.com>
2026-01-24 18:02:14 +0530

    feat: add simple request logging middleware to backend

--- src/server.js
+++ src/server.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Simple logging middleware
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+
 // Serve static files from the current directory
 app.use(express.static(__dirname));
 
