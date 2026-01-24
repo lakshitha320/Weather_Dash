35ce8370c3f228d059a08498bb06eab64f911e74
udeeshaakshari-art <udeeshaakshari-art@users.noreply.github.com>
2026-01-24 09:26:57 +0000

    deploy: 66c52b0e9f0bad96aea16402a559bebd91aa0fda

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Import weather routes
+const weatherRoutes = require("./routes/weather");
+app.use("/api/weather", weatherRoutes);
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Backend running on port ${PORT}`);
+});
